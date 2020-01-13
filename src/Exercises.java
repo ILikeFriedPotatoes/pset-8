e5f456e9a0454f8a0eb45033e6c2682f065b1640
Joseph Wang <jwang@ucvts.tec.nj.us>
2020-01-13 08:27:23 -0500

    Finished exercise 1

--- src/Exercises.java
+++ src/Exercises.java
@@ -3,8 +3,18 @@ public class Exercises {
 	public boolean commonEnd(int[] a, int[] b) {
 		if (a == null || a.length == 0 || b == null || b.length == 0) {
 			return false;
+		} else if(a.length < 1 || b.length < 1) {
+			return false;
+		} else if(a[0] == b[0]) {
+			return true;
+		} else if(a[a.length - 1] == b[b.length - 1]) {
+			return true;
+		} else {
+			return false;
 		}
 		
 		// write your code here
 	}
+	
+	
 }
